957ed20fc2c6cdcc7400fbe79e34b4aa8c4a13b8
frank <liuguozhu720@gmail.com>
2025-03-03 12:48:22 +0800

    700 completed

--- 700 Search in a Binary Search Tree/700 Search in a Binary Search Tree.cpp
+++ 700 Search in a Binary Search Tree/700 Search in a Binary Search Tree.cpp
@@ -1,24 +1,56 @@
+#include "../BinaryTreeUtil/binary_tree_util.h"
+#include "../PerformanceMonitor/performance_monitor.h"
+#include <cassert>
 #include <iostream>
+#include <string>
 #include <vector>
+
 using namespace std;
 
-class Solution {
-public:
-	struct TreeNode {
-		int val;
-		TreeNode* left;
-		TreeNode* right;
-		TreeNode() : val(0), left(nullptr), right(nullptr) {}
-		TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-		TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
-	};
+class Solution
+{
+  public:
+    TreeNode *searchBST(TreeNode *root, int val)
+    {
+        if (root == nullptr) {
+            return root;
+        }
 
-	TreeNode* searchBST(TreeNode* root, int val) {
-
-	}
+        if (val == root->val)
+            return root;
+        else if (val < root->val) {
+            return searchBST(root->left, val);
+        } else {
+            return searchBST(root->right, val);
+        }
+    }
 };
 
 int main()
 {
-	std::cout << "Hello World!\n";
+
+    Solution           solution;
+    PerformanceMonitor monitor;
+
+    // Input: root = [4,2,7,1,3], val = 2
+    vector<int> nums1 = {4, 2, 7, 1, 3};
+    int         val1  = 2;
+    auto        root1 = BinaryTreeUtil::buildTree(nums1);
+    monitor.start();
+    auto res1 = solution.searchBST(root1, val1);
+    monitor.stop();
+    BinaryTreeUtil::printTree(res1);
+    // Output: [2,1,3]
+
+    // Input: root = [4,2,7,1,3], val = 5
+    vector<int> nums2 = {4, 2, 7, 1, 3};
+    int         val2  = 5;
+    auto        root2 = BinaryTreeUtil::buildTree(nums2);
+    monitor.start();
+    auto res2 = solution.searchBST(root2, val2);
+    monitor.stop();
+    BinaryTreeUtil::printTree(res2);
+    // Output: []
+
+    return 0;
 }
