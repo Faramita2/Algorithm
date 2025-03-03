e0702cd85403f55af0767b517b6127f71aa1f85c
frank <liuguozhu720@gmail.com>
2025-03-03 15:59:04 +0800

    530 completed

--- 530 Minimum Absolute Difference in BST/530 Minimum Absolute Difference in BST.cpp
+++ 530 Minimum Absolute Difference in BST/530 Minimum Absolute Difference in BST.cpp
@@ -1,24 +1,58 @@
+#include "../BinaryTreeUtil/binary_tree_util.h"
+#include "../PerformanceMonitor/performance_monitor.h"
+#include <cassert>
 #include <iostream>
+#include <string>
 #include <vector>
+
 using namespace std;
 
-class Solution {
-public:
-	struct TreeNode {
-		int val;
-		TreeNode* left;
-		TreeNode* right;
-		TreeNode() : val(0), left(nullptr), right(nullptr) {}
-		TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-		TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
-	};
+class Solution
+{
+  public:
+    int getMinimumDifference(TreeNode *root)
+    {
+        int minD     = INT_MAX;
+        int preValue = -1;
+        lnr(root, minD, preValue);
+        return minD;
+    }
 
-	int getMinimumDifference(TreeNode* root) {
+  private:
+    void lnr(TreeNode *node, int &minD, int &preValue)
+    {
+        if (node == nullptr)
+            return;
 
-	}
+        lnr(node->left, minD, preValue);
+
+        if (preValue != -1)
+            minD = min(node->val - preValue, minD);
+
+        preValue = node->val;
+        lnr(node->right, minD, preValue);
+    }
 };
 
 int main()
 {
-	std::cout << "Hello World!\n";
+
+    Solution           solution;
+    PerformanceMonitor monitor;
+
+    // Input: root = [4,2,6,1,3]
+    vector<int> nums1 = {5, 4, 7};
+    auto        root1 = BinaryTreeUtil::buildTree(nums1);
+    BinaryTreeUtil::printTree(root1);
+    assert(1 == solution.getMinimumDifference(root1));
+    // Output: 1
+
+    // Input: root = [1,0,48,null,null,12,49]
+    vector<int> nums2 = {1, 0, 48, -1, -1, 12, 49};
+    auto        root2 = BinaryTreeUtil::buildTree(nums2);
+    BinaryTreeUtil::printTree(root2);
+    assert(1 == solution.getMinimumDifference(root2));
+    // Output: 1
+
+    return 0;
 }
