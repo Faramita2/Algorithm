8f0b2e4991a4e086f6a511849f0e3804ae22290d
frank <liuguozhu720@gmail.com>
2025-03-03 20:55:15 +0800

    701 completed

--- 701 Insert into a Binary Search Tree/701 Insert into a Binary Search Tree.cpp
+++ 701 Insert into a Binary Search Tree/701 Insert into a Binary Search Tree.cpp
@@ -1,24 +1,57 @@
+#include "../BinaryTreeUtil/binary_tree_util.h"
+#include "../PerformanceMonitor/performance_monitor.h"
+#include <cassert>
 #include <iostream>
+#include <string>
 #include <vector>
+
 using namespace std;
 
-class Solution {
-public:
-	struct TreeNode {
-		int val;
-		TreeNode* left;
-		TreeNode* right;
-		TreeNode() : val(0), left(nullptr), right(nullptr) {}
-		TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-		TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
-	};
+class Solution
+{
+  public:
+    TreeNode *insertIntoBST(TreeNode *root, int val)
+    {
+        if (root == nullptr) {
+            return new TreeNode(val);
+        }
+        if (val < root->val)
+            root->left = insertIntoBST(root->left, val);
+        else
+            root->right = insertIntoBST(root->right, val);
 
-	TreeNode* insertIntoBST(TreeNode* root, int val) {
-
-	}
+        return root;
+    }
 };
 
 int main()
 {
-	std::cout << "Hello World!\n";
+    Solution           solution;
+    PerformanceMonitor monitor;
+
+    // Input: root = [4,2,7,1,3], val = 5
+    vector<int> nums1         = {4, 2, 7, 1, 3};
+    int         val1          = 5;
+    auto        root1         = BinaryTreeUtil::buildTree(nums1);
+    auto        insertedRoot1 = solution.insertIntoBST(root1, val1);
+    BinaryTreeUtil::printTree(insertedRoot1);
+    // Output: [4,2,7,1,3,5]
+
+    // Input: root = [40,20,60,10,30,50,70], val = 25
+    vector<int> nums2         = {40, 20, 60, 10, 30, 50, 70};
+    int         val2          = 25;
+    auto        root2         = BinaryTreeUtil::buildTree(nums2);
+    auto        insertedRoot2 = solution.insertIntoBST(root2, val2);
+    BinaryTreeUtil::printTree(insertedRoot2);
+    // Output: [40,20,60,10,30,50,70,-1,-1,25]
+
+    // Input: root = [4,2,7,1,3,-1,-1,-1,-1,-1,-1], val = 5
+    vector<int> nums3         = {4, 2, 7, 1, 3, -1, -1, -1, -1, -1, -1};
+    int         val3          = 5;
+    auto        root3         = BinaryTreeUtil::buildTree(nums3);
+    auto        insertedRoot3 = solution.insertIntoBST(root3, val3);
+    BinaryTreeUtil::printTree(insertedRoot3);
+    // Output: [4,2,7,1,3,5]
+
+    return 0;
 }
