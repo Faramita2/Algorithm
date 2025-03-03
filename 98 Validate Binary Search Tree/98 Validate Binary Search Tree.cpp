d044c7704140951af8c4b177c4f7003f077ac892
frank <liuguozhu720@gmail.com>
2025-03-03 14:59:25 +0800

    98 completed

--- 98 Validate Binary Search Tree/98 Validate Binary Search Tree.cpp
+++ 98 Validate Binary Search Tree/98 Validate Binary Search Tree.cpp
@@ -1,24 +1,49 @@
+#include "../BinaryTreeUtil/binary_tree_util.h"
+#include "../PerformanceMonitor/performance_monitor.h"
+#include <cassert>
 #include <iostream>
+#include <string>
 #include <vector>
+
 using namespace std;
 
-class Solution {
-public:
-	struct TreeNode {
-		int val;
-		TreeNode* left;
-		TreeNode* right;
-		TreeNode() : val(0), left(nullptr), right(nullptr) {}
-		TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-		TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
-	};
+class Solution
+{
+    TreeNode *pre;
 
-	bool isValidBST(TreeNode* root) {
-
-	}
+  public:
+    bool isValidBST(TreeNode *root)
+    {
+        if (root != nullptr) {
+            if (!isValidBST(root->left))
+                return false;
+            if (pre != nullptr && pre->val >= root->val)
+                return false;
+            pre = root;
+            if (!isValidBST(root->right))
+                return false;
+        }
+        return true;
+    }
 };
 
 int main()
 {
-	std::cout << "Hello World!\n";
+
+    Solution           solution;
+    PerformanceMonitor monitor;
+
+    // Input: root = [2,1,3]
+    vector<int> nums1 = {2, 1, 3};
+    auto        root1 = BinaryTreeUtil::buildTree(nums1);
+    assert(true == solution.isValidBST(root1));
+    // Output: true;
+
+    // Input: root = [5,1,4,null,null,3,6]
+    vector<int> nums2 = {5, 1, 4, -1, -1, 3, 6};
+    auto        root2 = BinaryTreeUtil::buildTree(nums2);
+    assert(false == solution.isValidBST(root2));
+    // Output: false;
+
+    return 0;
 }
