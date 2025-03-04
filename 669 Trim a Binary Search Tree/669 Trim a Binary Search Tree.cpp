66b92640ebbe80f2c0b2100ddcdee0bfb8c4d7c1
frank <liuguozhu720@gmail.com>
2025-03-04 17:27:52 +0800

    669 completed

--- 669 Trim a Binary Search Tree/669 Trim a Binary Search Tree.cpp
+++ 669 Trim a Binary Search Tree/669 Trim a Binary Search Tree.cpp
@@ -1,24 +1,57 @@
+#include "../BinaryTreeUtil/binary_tree_util.h"
+#include "../PerformanceMonitor/performance_monitor.h"
+#include <cassert>
 #include <iostream>
+#include <string>
 #include <vector>
+
 using namespace std;
 
-class Solution {
-public:
-	struct TreeNode {
-		int val;
-		TreeNode* left;
-		TreeNode* right;
-		TreeNode() : val(0), left(nullptr), right(nullptr) {}
-		TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-		TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
-	};
+class Solution
+{
+  public:
+    TreeNode *trimBST(TreeNode *root, int low, int high)
+    {
+        if (root == nullptr)
+            return nullptr;
 
-	TreeNode* trimBST(TreeNode* root, int low, int high) {
+        if (root->val < low) {
+            return trimBST(root->right, low, high);
+        }
 
-	}
+        if (root->val > high) {
+            return trimBST(root->left, low, high);
+        }
+
+        root->left  = trimBST(root->left, low, high);
+        root->right = trimBST(root->right, low, high);
+
+        return root;
+    }
 };
 
 int main()
 {
-	std::cout << "Hello World!\n";
+    Solution           solution;
+    PerformanceMonitor monitor;
+
+    // Input: root = [1,0,2], low = 1, high = 2
+    vector<int> nums1       = {1, 0, 2};
+    int         low1        = 1;
+    int         high1       = 2;
+    auto        root1       = BinaryTreeUtil::buildTree(nums1);
+    auto        trimedRoot1 = solution.trimBST(root1, low1, high1);
+    BinaryTreeUtil::printTree(trimedRoot1);
+    // Output: [1,-1,2]
+
+    // Input: root = [3,0,4,-1,2,-1,-1,1], low = 1, high = 3
+    vector<int> nums2       = {3, 0, 4, -1, 2, -1, -1, 1};
+    int         low2        = 1;
+    int         high2       = 3;
+    auto        root2       = BinaryTreeUtil::buildTree(nums2);
+    auto        trimedRoot2 = solution.trimBST(root2, low2, high2);
+    BinaryTreeUtil::printTree(trimedRoot2);
+    // Output: [3,2,-1,1]
+
+    return 0;
 }
