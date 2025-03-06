c708b5c4c963ea923f760a0c5cd7042eea69cf3c
frank <liuguozhu720@gmail.com>
2025-03-06 17:47:06 +0800

    538 completed

--- 538 Convert BST to Greater Tree/538 Convert BST to Greater Tree.cpp
+++ 538 Convert BST to Greater Tree/538 Convert BST to Greater Tree.cpp
@@ -1,24 +1,52 @@
+#include "../BinaryTreeUtil/binary_tree_util.h"
+#include "../PerformanceMonitor/performance_monitor.h"
+#include <cassert>
 #include <iostream>
+#include <string>
 #include <vector>
+
 using namespace std;
 
-class Solution {
-public:
-	struct TreeNode {
-		int val;
-		TreeNode* left;
-		TreeNode* right;
-		TreeNode() : val(0), left(nullptr), right(nullptr) {}
-		TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-		TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
-	};
+class Solution
+{
+  public:
+    TreeNode *convertBST(TreeNode *root)
+    {
+        int curSum = 0;
+        rnl(root, curSum);
+        return root;
+    }
 
-	TreeNode* convertBST(TreeNode* root) {
-
-	}
+  private:
+    void rnl(TreeNode *root, int &curSum)
+    {
+        if (root == nullptr)
+            return;
+        rnl(root->right, curSum);
+        curSum += root->val;
+        root->val = curSum;
+        rnl(root->left, curSum);
+    }
 };
 
 int main()
 {
-	std::cout << "Hello World!\n";
+    Solution           solution;
+    PerformanceMonitor monitor;
+
+    // Input: root = [4,1,6,0,2,5,7,-1,-1,-1,3,-1,-1,-1,8]
+    vector<int> nums1          = {4, 1, 6, 0, 2, 5, 7, -1, -1, -1, 3, -1, -1, -1, 8};
+    auto        root1          = BinaryTreeUtil::buildTree(nums1);
+    auto        convertedRoot1 = solution.convertBST(root1);
+    BinaryTreeUtil::printTree(convertedRoot1);
+    // Output: [30,36,21,36,35,26,15,null,null,null,33,null,null,null,8]
+
+    // Input: root = [0,null,1]
+    vector<int> nums2          = {0, -1, 1};
+    auto        root2          = BinaryTreeUtil::buildTree(nums2);
+    auto        convertedRoot2 = solution.convertBST(root2);
+    BinaryTreeUtil::printTree(convertedRoot2);
+    // Output: [1,null,1]
+
+    return 0;
 }
