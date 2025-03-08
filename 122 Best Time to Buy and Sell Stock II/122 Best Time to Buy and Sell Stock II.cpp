1a5882848ea8c35227fd3e116e202f85fa8a0217
frank <liuguozhu720@gmail.com>
2025-03-09 07:46:27 +0800

    122 completed

--- 122 Best Time to Buy and Sell Stock II/122 Best Time to Buy and Sell Stock II.cpp
+++ 122 Best Time to Buy and Sell Stock II/122 Best Time to Buy and Sell Stock II.cpp
@@ -1,15 +1,44 @@
+#include "../BinaryTreeUtil/binary_tree_util.h"
+#include "../PerformanceMonitor/performance_monitor.h"
+#include <cassert>
 #include <iostream>
+#include <string>
 #include <vector>
+
 using namespace std;
 
-class Solution {
-public:
-    int maxProfit(vector<int>& prices) {
-
+class Solution
+{
+  public:
+    int maxProfit(vector<int> &prices)
+    {
+        int res = 0;
+        for (int i = 1; i < prices.size(); i++) {
+            res += max(prices[i] - prices[i - 1], 0);
+        }
+        return res;
     }
 };
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Solution           solution;
+    PerformanceMonitor monitor;
+
+    // Input: prices = [7,1,5,3,6,4]
+    vector<int> prices1 = {7, 1, 5, 3, 6, 4};
+    cout << solution.maxProfit(prices1) << endl;
+    // Output: 7
+
+    // Input: prices = [1,2,3,4,5]
+    vector<int> prices2 = {1, 2, 3, 4, 5};
+    cout << solution.maxProfit(prices2) << endl;
+    // Output: 4
+
+    // Input: prices = [7,6,4,3,1]
+    vector<int> prices3 = {7, 6, 4, 3, 1};
+    cout << solution.maxProfit(prices3) << endl;
+    // Output: 0
+
+    return 0;
 }
