aca79996b9ccb30ac3d6e1846805772b04d95c9b
frank <liuguozhu720@gmail.com>
2025-03-09 10:26:42 +0800

    70 completed

--- 70 Climbing Stairs/70 Climbing Stairs.cpp
+++ 70 Climbing Stairs/70 Climbing Stairs.cpp
@@ -1,13 +1,45 @@
+#include "../BinaryTreeUtil/binary_tree_util.h"
+#include "../PerformanceMonitor/performance_monitor.h"
+#include <cassert>
 #include <iostream>
+#include <string>
+#include <vector>
 
-class Solution {
-public:
-    int climbStairs(int n) {
+using namespace std;
 
+class Solution
+{
+  public:
+    int climbStairs(int n)
+    {
+        if (n == 1 || n == 2)
+            return n;
+        vector<int> dp(n + 1, 0);
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 1; i <= n; i++) {
+            if (i < 3)
+                continue;
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
     }
 };
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Solution           solution;
+    PerformanceMonitor monitor;
+
+    // Input: n = 2
+    int n1 = 2;
+    cout << solution.climbStairs(n1) << endl;
+    // Output: 2
+
+    // Input: n = 3
+    int n2 = 3;
+    cout << solution.climbStairs(n2) << endl;
+    // Output: 3
+
+    return 0;
 }
