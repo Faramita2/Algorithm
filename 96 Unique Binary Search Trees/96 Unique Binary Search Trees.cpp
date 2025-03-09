0315bc2b259e62bccfd7fd7ded0c7d2c4dc2bb37
frank <liuguozhu720@gmail.com>
2025-03-09 16:32:14 +0800

    96 completed

--- 96 Unique Binary Search Trees/96 Unique Binary Search Trees.cpp
+++ 96 Unique Binary Search Trees/96 Unique Binary Search Trees.cpp
@@ -1,13 +1,47 @@
+#include "../BinaryTreeUtil/binary_tree_util.h"
+#include "../PerformanceMonitor/performance_monitor.h"
+#include <cassert>
 #include <iostream>
+#include <string>
+#include <vector>
 
-class Solution {
-public:
-    int numTrees(int n) {
+using namespace std;
 
+class Solution
+{
+  public:
+    int numTrees(int n)
+    {
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] += dp[i - j] * dp[j - 1];
+            }
+        }
+
+        for (auto v : dp) {
+            v *= 2;
+            cout << v << " ";
+        }
+        cout << endl;
+        return dp[n];
     }
 };
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Solution           solution;
+    PerformanceMonitor monitor;
+
+    // Input: n = 3
+    int n1 = 3;
+    cout << solution.numTrees(n1) << endl;
+    // Output: 5
+
+    int n2 = 1;
+    cout << solution.numTrees(n2) << endl;
+    // Output: 1
+
+    return 0;
 }
