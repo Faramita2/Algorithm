acc65572db45374482d4ccc2bc93d248c996593b
frank <liuguozhu720@gmail.com>
2025-03-10 00:23:44 +0800

    416 completed

--- 416 Partition Equal Subset Sum/416 Partition Equal Subset Sum.cpp
+++ 416 Partition Equal Subset Sum/416 Partition Equal Subset Sum.cpp
@@ -1,15 +1,51 @@
+#include "../BinaryTreeUtil/binary_tree_util.h"
+#include "../PerformanceMonitor/performance_monitor.h"
+#include <cassert>
 #include <iostream>
+#include <string>
 #include <vector>
+
 using namespace std;
 
-class Solution {
-public:
-    bool canPartition(vector<int>& nums) {
+class Solution
+{
+  public:
+    bool canPartition(vector<int> &nums)
+    {
+        int total = 0;
+        for (int i : nums) {
+            total += i;
+        }
+        if (total % 2 == 1)
+            return false;
+        int target = total / 2;
 
+        vector<bool> dp(target + 1, false);
+        dp[0] = true;
+        for (int num : nums) {
+            for (int j = target; j >= num; j--) {
+                dp[j] = dp[j] || dp[j - num];
+            }
+        }
+
+        return dp[target];
     }
 };
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Solution           solution;
+    PerformanceMonitor monitor;
+
+    // Input: nums = [1,5,11,5]
+    vector<int> nums1 = {1, 5, 11, 5};
+    cout << solution.canPartition(nums1) << endl;
+    // Output: true;
+
+    // Input: nums = [1,2,3,5]
+    vector<int> nums2 = {1, 2, 3, 5};
+    cout << solution.canPartition(nums2) << endl;
+    // Output: false
+
+    return 0;
 }
