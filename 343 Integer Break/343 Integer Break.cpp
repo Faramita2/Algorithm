ee23b6c192ff019529d52beab55ac4a9a07065ce
frank <liuguozhu720@gmail.com>
2025-03-09 16:03:34 +0800

    343 completed

--- 343 Integer Break/343 Integer Break.cpp
+++ 343 Integer Break/343 Integer Break.cpp
@@ -1,13 +1,48 @@
+#include "../BinaryTreeUtil/binary_tree_util.h"
+#include "../PerformanceMonitor/performance_monitor.h"
+#include <cassert>
 #include <iostream>
+#include <string>
+#include <vector>
 
-class Solution {
-public:
-    int integerBreak(int n) {
+using namespace std;
 
+class Solution
+{
+  public:
+    int integerBreak(int n)
+    {
+        vector<int> dp(n + 1, 0);
+        dp[2] = 1;
+        for (int i = 3; i < n + 1; i++) {
+            for (int j = 1; j <= i / 2; j++) {
+                dp[i] = max(dp[i], max(j * dp[i - j], j * (i - j)));
+            }
+        }
+
+        for (int i : dp) {
+            cout << i << " ";
+        }
+        cout << endl;
+
+        return dp[n];
     }
 };
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Solution           solution;
+    PerformanceMonitor monitor;
+
+    // Input: n = 2
+    int n1 = 2;
+    cout << solution.integerBreak(n1) << endl;
+    // Output: 1
+
+    // Input: n = 10
+    int n2 = 10;
+    cout << solution.integerBreak(n2) << endl;
+    // Output: 36
+
+    return 0;
 }
