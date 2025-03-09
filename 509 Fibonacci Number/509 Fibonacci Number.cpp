b564e237de2ef4872aa1dc514153cfe0ecee6aaf
frank <liuguozhu720@gmail.com>
2025-03-09 08:56:26 +0800

    509 completed

--- 509 Fibonacci Number/509 Fibonacci Number.cpp
+++ 509 Fibonacci Number/509 Fibonacci Number.cpp
@@ -1,13 +1,50 @@
+#include "../BinaryTreeUtil/binary_tree_util.h"
+#include "../PerformanceMonitor/performance_monitor.h"
+#include <cassert>
 #include <iostream>
+#include <string>
+#include <vector>
 
-class Solution {
-public:
-    int fib(int n) {
+using namespace std;
 
+class Solution
+{
+  public:
+    int fib(int n)
+    {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        vector<int> dp(n + 1);
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i < n + 1; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
     }
 };
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Solution           solution;
+    PerformanceMonitor monitor;
+
+    // Input: n = 2
+    int n1 = 2;
+    cout << solution.fib(n1) << endl;
+    // Output: 1
+
+    // Input: n = 3
+    int n2 = 3;
+    cout << solution.fib(n2) << endl;
+    // Output: 2
+
+    // Input: n = 4
+    int n3 = 4;
+    cout << solution.fib(n3) << endl;
+    // Output: 3
+
+    return 0;
 }
