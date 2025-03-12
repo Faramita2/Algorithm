3119c9280399a0d47e2a0955e0ee003d4131050c
frank <liuguozhu720@gmail.com>
2025-03-12 12:06:30 +0800

    198 completed

--- 198 House Robber/198 House Robber.cpp
+++ 198 House Robber/198 House Robber.cpp
@@ -1,15 +1,49 @@
+#include "../BinaryTreeUtil/binary_tree_util.h"
+#include "../PerformanceMonitor/performance_monitor.h"
+#include <cassert>
+#include <fstream>
 #include <iostream>
+#include <sstream>
+#include <string>
 #include <vector>
+
 using namespace std;
 
-class Solution {
-public:
-    int rob(vector<int>& nums) {
+class Solution
+{
+  public:
+    int rob(vector<int> &nums)
+    {
+        int size = nums.size();
+        if (size == 1)
+            return nums.front();
+        if (size == 2)
+            return max(nums.front(), nums.back());
+        vector<int> dp(size, 0);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
 
+        for (int i = 2; i < size; i++) {
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+        return dp.back();
     }
 };
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Solution           solution;
+    PerformanceMonitor monitor;
+
+    // Input: nums = [1,2,3,1]
+    vector<int> nums1 = {1, 2, 3, 1};
+    cout << solution.rob(nums1) << endl;
+    // Output: 4
+
+    // Input: nums = [2,7,9,3,1]
+    vector<int> nums2 = {2, 7, 9, 3, 1};
+    cout << solution.rob(nums2) << endl;
+    // Output: 12
+
+    return 0;
 }
