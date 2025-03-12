38a35303ad293dedff0db718b14d01be38a95da4
frank <liuguozhu720@gmail.com>
2025-03-12 11:22:37 +0800

    279

--- 279 Perfect Squares/279 Perfect Squares.cpp
+++ 279 Perfect Squares/279 Perfect Squares.cpp
@@ -1,13 +1,49 @@
+#include "../BinaryTreeUtil/binary_tree_util.h"
+#include "../PerformanceMonitor/performance_monitor.h"
+#include <cassert>
+#include <cmath>
+#include <fstream>
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
 
-class Solution {
-public:
-    int numSquares(int n) {
+using namespace std;
 
+class Solution
+{
+  public:
+    int numSquares(int n)
+    {
+        if (n == 1)
+            return 1;
+        vector<int> dp(n + 1, INT_MAX);
+        dp[0] = 0;
+        for (int i = 1; i <= sqrt(n); i++) {
+            for (int j = i * i; j <= n; j++) {
+                if (dp[j - i * i] != INT_MAX) {
+                    dp[j] = min(dp[j], dp[j - i * i] + 1);
+                }
+            }
+        }
+        return dp.back();
     }
 };
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Solution           solution;
+    PerformanceMonitor monitor;
+
+    // Input: n = 12
+    int n1 = 12;
+    cout << solution.numSquares(n1) << endl;
+    // Output: 3
+
+    // Input: n = 13
+    int n2 = 13;
+    cout << solution.numSquares(n2) << endl;
+    // Output: 2
+
+    return 0;
 }
